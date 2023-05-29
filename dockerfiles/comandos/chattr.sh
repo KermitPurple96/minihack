e9b1878645676017b648924249dec1b8100353ca
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-05-29 23:41:50 +0200

    Add files via upload

---
+++ dockerfiles/comandos/chattr.sh
@@ -0,0 +1,4 @@
+sudo chattr +i /home/mini1/.flag
+sudo chattr +i /home/mini2/-
+sudo chattr +i /home/mini3/challenge_3.txt
+sudo chattr +i /home/mini4/challenge_4.txt
---
+++ dockerfiles/usuarios/chattr.sh
@@ -0,0 +1,3 @@
+sudo chattr +i /home/mini1/challenge_1.txt
+sudo chattr +i /home/mini2/challenge_2.txt
+sudo chattr +i /home/mini3/challenge_3.txt
