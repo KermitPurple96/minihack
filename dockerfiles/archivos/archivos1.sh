f2e3a2a0042482134cfdb51ab157bcb747b7dbf2
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-05-29 23:39:20 +0200

    Delete dockerfiles directory

--- dockerfiles/archivos/archivos1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-echo "Flag -> 9N3TKbA8rXnIpe40Dz5Q1oLRa" >> /etc/hosts
--- dockerfiles/entorno/chattr.sh
+++
@@ -1,5 +0,0 @@
-sudo chattr +i /home/mini1/challenge_1.txt
-sudo chattr +i /home/mini2/challenge_2.txt
-sudo chattr +i /home/mini3/challenge_3a.txt
-sudo chattr +i /home/mini3/challenge_3b.txt
-sudo chattr +i /home/mini4/challenge_4.txt
