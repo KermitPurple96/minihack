295b36480ad72ceae598b495dbdb3ca7405db3b3
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-06-01 00:01:43 +0200

    Add files via upload

---
+++ static/js/comandos.js
@@ -0,0 +1,33 @@
+
+
+//     var botones = document.getElementsByClassName("barcom");
+//     console.log(botones)
+
+//     for (var j = 0; j < botones.length; j++) {
+//         var boton = botones[j];
+//         var target = boton.getAttribute("id");
+//         boton.addEventListener("click", function() {
+//             comando_hide(target);
+//         });
+//     }
+
+        
+
+
+
+// let comando_hide(target){
+
+//     let elements = document.getElementsByClassName("com_div");
+//     for(let i = 0; i < elements.length; i++){
+//         if(elements[i].id.includes(target)){
+//             elements[i].style.display = "block"
+//         }
+//         else{
+//             elements[i].style.display = "none"
+//         }
+//     }
+
+// }
+
+
+
